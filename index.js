dab2dda959be675df34a67d7c77325636bf9da7d
bmukha <47007157+bmukha@users.noreply.github.com>
2021-08-14 19:39:44 +0300

    heroku commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-  res.end("Hi there! I'm your server and I'm responding!");
+  res.end("Hi there! I'm your server and I'm responding on Heroku!");
 });
 
 server.listen(3000, "localhost");
