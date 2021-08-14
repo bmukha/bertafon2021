474e471d1346f1d6b41206d9e2743a17c9c064c9
bmukha <47007157+bmukha@users.noreply.github.com>
2021-08-14 19:53:43 +0300

    second heroku  commit

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const http = require("http");
+const express = reqire("express");
+
+const app = express()
+
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
