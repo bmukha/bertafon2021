f47b433e2551e9677efc07366d8c155a051a708b
bmukha <47007157+bmukha@users.noreply.github.com>
2021-08-14 19:29:40 +0300

    server is responding

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  res.end("Hi there! I'm your server and I'm responding!");
+});
+
+server.listen(3000, "localhost");
