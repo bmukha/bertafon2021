870f379bd0cb96c0a6b0dd6fe5fb4ab36f027cc2
bmukha <47007157+bmukha@users.noreply.github.com>
2021-10-03 16:30:08 +0300

    heroku viber say winston deleted

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 const ViberBot = require("viber-bot").Bot;
 const BotEvents = require("viber-bot").Events;
 const TextMessage = require("viber-bot").Message.Text;
-const winston = require("winston");
-const toYAML = require("winston-console-formatter");
-var request = require("request");
-
-function createLogger() {
-  const logger = new winston.Logger({
-    level: "debug", // We recommend using the debug level for development
-  });
-
-  logger.add(winston.transports.Console, toYAML.config());
-  return logger;
-}
-
-const logger = createLogger();
+let request = require("request");
 
 // Creating the bot with access token, name and avatar
-const bot = new ViberBot(logger, {
+const bot = new ViberBot({
   authToken: "4dd1b0a38027d01f-c2a15cd28744ac5c-3784344e9199cec8",
   name: "Is It Up", // <--- Your bot name here
   avatar: "http://api.adorable.io/avatar/200/isitup", // It is recommended to be 720x720, and no more than 100kb.
@@ -27,16 +14,11 @@ if (process.env.NOW_URL || process.env.HEROKU_URL) {
   const http = require("http");
   const port = process.env.PORT || 8080;
 
-
   http
     .createServer(bot.middleware())
     .listen(port, () =>
       bot.setWebhook(process.env.NOW_URL || process.env.HEROKU_URL)
     );
-} else {
-  logger.debug(
-    "Could not find the now.sh/Heroku environment variables. Please make sure you followed readme guide."
-  );
 }
 
 function say(response, message) {
